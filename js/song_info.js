f264ee6ab86ab30b1e6f599a427d8554c0820162
cksdnd1232@gmail.com <cksdnd1232@gmail.com>
2025-07-12 00:16:59 +0900

    07.12 웹 작업

--- js/song_info.js
+++ js/song_info.js
@@ -1,3 +1,7 @@
-function search() {
+document.getElementById("search").addEventListener("change", (e) => {
     alert("추후 서비스 제공 예정");
-}
+    document.getElementById("search").value = "";
+});
+document.getElementById("nav").addEventListener("click", (e) => {
+    alert("추후 서비스 제공 예정");
+});
