9786b98fd7d48a398a16554c60fbb405607aec0d
cksdnd1232@gmail.com <cksdnd1232@gmail.com>
2025-07-13 03:50:03 +0900

    07.13 준 완성

--- js/song_info.js
+++ js/song_info.js
@@ -2,6 +2,7 @@
 document.getElementById("search").addEventListener("change", (e) => {
     alert("추후 서비스 제공 예정");
     document.getElementById("search").value = "";
+    document.getElementById("search").focus;
 });
 //내비게이션 클릭했을 때 알림
 document.getElementById("nav").addEventListener("click", (e) => {
