af94b8c3ef9660c7ea418d69732707fb28800c91
yuzu <gks940122@gmail.com>
2025-07-14 23:40:35 +0900

    플리 업로드

--- js/playlist.js
+++ js/playlist.js
@@ -1,7 +1,7 @@
-document.getElementById("unable").onclick = function() {
-    alert("해당 기능은 추후 업데이트 예정입니다.");
-}
-
 function search() {
     alert("해당 기능은 추후 업데이트 예정입니다.");
 }
+
+document.getElementById("join").addEventListener("click", () => {
+    alert("신청 기간이 아닙니다.")
+});
