f7857459a0f8cd1c663b6d0ce5845e19e6fe9b70
yyuzu1 <gks940122@gmail.com>
2025-07-15 14:18:17 +0900

    커밋

--- js/playlist.js
+++ js/playlist.js
@@ -5,7 +5,3 @@ function search() {
 document.getElementById("join").addEventListener("click", () => {
     alert("신청 기간이 아닙니다.");
 });
-
-document.getElementById("unable").onclick = function () {
-    alert("해당 기능은 추후 업데이트 예정입니다.");
-};
