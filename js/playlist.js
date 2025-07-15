ec66722f1c4c24f2a2d9a1df743b53616bd768d2
yyuzu1 <gks940122@gmail.com>
2025-07-15 17:46:14 +0900

    찐찐찐찐 최종본

--- js/playlist.js
+++ js/playlist.js
@@ -5,3 +5,19 @@ function search() {
 document.getElementById("join").addEventListener("click", () => {
     alert("신청 기간이 아닙니다.");
 });
+
+//좋아요 눌렀을 때 하트 색깔, 좋아요 갯수 + 1 or 한번 더 누르면 - 1
+document.getElementById("like").addEventListener("click", (e) => {
+    const icon = document.getElementById("likeIcon");
+    let likedSongCount = Number(document.getElementById("likeNumber").textContent);
+    
+    if (icon.innerHTML == '🤍') {
+        icon.innerHTML = '❤️';
+        likedSongCount++;
+    } else {
+        icon.innerHTML = '🤍'
+        likedSongCount--;
+    }
+
+    document.getElementById("likeNumber").textContent = likedSongCount;
+});
